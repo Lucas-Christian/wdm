80590f8a4ed71728e2a7f1248f8ffcaece58a82a
Lucas Christian <lucas.christian.programmer@gmail.com>
2023-05-04 13:29:20 -0300

    Updated

--- learning_typescript/src/index.ts
+++ learning_typescript/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+enum Weekday {
+  Sunday = 0,
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6,
+}
+
+console.log(Weekday.Monday); // 1
