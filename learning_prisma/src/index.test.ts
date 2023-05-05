6bd683300ed399f77395ad325c8e1111fc8177e5
Lucas Christian <lucas.christian.programmer@gmail.com>
2023-05-05 15:04:13 -0300

    Updated

--- learning_prisma/src/index.test.ts
+++ learning_prisma/src/index.test.ts
@@ -1,8 +1,89 @@
-import { describe, expect, test } from "vitest";
-import { helloWorld } from "./index"; 
-
-describe("#helloWorld", () => {
-  test("É esperado que seja retornada a string 'Hello World!'", () => {
-    expect(helloWorld()).toBe("Hello World!");
+import { describe, test, expect, beforeAll } from "vitest";
+import { prisma, createSingleRecord, findSingleRecord, findAllRecords, updateSingleRecord, updateManyRecords, updateOrCreate, deleteSingleRecord, deleteManyRecords, deleteAllRecords, createManyRecords } from "./index"; 
+describe("#praticando", () => {
+  test("É esperado que seja criado um usuário no banco de dados sem gerar erros.", async () => {
+    try {
+      let user = await createSingleRecord();
+      console.log(user);
+      console.log("Usuário criado com sucesso.");
+    } catch(err) {
+      console.log("O usuário já foi criado.");
+    }
+  });
+  test("É esperado que retorne o usuário chamado O Curso", async () => {
+    try {
+      let user = await findSingleRecord("share.this.course@gmail.com");
+      expect(user?.name === "O Curso").toBe(true);
+      console.log(user)
+      console.log("Sucesso ao buscar pelo usuário");
+    } catch(err) {
+      console.log("Erro ao buscar pelo usuário");
+    }
+  });
+  test("É esperado que todos os usuários sejam imprimidos no console", async () => {
+    try {
+      let users = await findAllRecords();
+      console.log(users);
+      console.log("Sucesso ao buscar por todos os usuários")
+    } catch(err) {
+      console.log("Falha ao buscar todos os usuários");
+    }
+  });
+  test("É esperado que o nome seja atualizado sem erros", async () => {
+    let user = await findSingleRecord("share.this.course@gmail.com");
+    console.log(user!.name);
+    await updateSingleRecord("share.this.course@gmail.com", "O CURSO");
+    user = await findSingleRecord("share.this.course@gmail.com");
+    console.log(user!.name);
+  });
+  test("É esperado que vários nomes sejam atualizados sem erros", async () => {
+    console.log(await findAllRecords());
+    await updateManyRecords();
+    console.log(await findAllRecords());
+  });
+  test("É esperado que o nome tente ser atualizado, caso não exista, ele cria.", async () => {
+    await updateOrCreate("teste@teste.com", "Teste");
+    console.log(await findSingleRecord("teste@teste.com"));
+    await updateOrCreate("teste@teste.com", "Testando");
+    console.log(await findSingleRecord("teste@teste.com"));
+  });
+  test("É esperado que exclua um registro", async () => {
+    let user = await findSingleRecord("share.this.course@gmail.com");
+    console.log(user!.name);
+    await deleteSingleRecord("share.this.course@gmail.com");
+    expect(await findSingleRecord("share.this.course@gmail.com")).toBe(null);
+  });
+  test("É esperado que exclua vários registros", async () => {
+    await createManyRecords();
+    try {
+      expect(
+        (await prisma.user.findMany({
+          where: {
+            email: {
+              contains: "@gmail.com"
+            }
+          }
+        })) !== null
+      ).toBe(true);
+      await deleteManyRecords();
+      expect(
+        await prisma.user.findMany({
+          where: {
+            email: {
+              contains: "@gmail.com"
+            }
+          }
+        })
+      ).toBe(null);
+    } catch(err) {
+      console.log("Erro ao tentar deletar todos os registros");
+    }
+  });
+  test("É esperado que exclua todos os registros", async () => {
+    await createManyRecords();
+    let users = await findAllRecords();
+    await deleteAllRecords();
+    users = await findAllRecords();
+    expect(users.length === 0).toBe(true);
   });
 });
--- learning_prisma/src/index.ts
+++ learning_prisma/src/index.ts
@@ -1,3 +1,107 @@
-export function helloWorld() {
-  return "Hello World!";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+export async function createSingleRecord() {
+  const user = await prisma.user.create({
+    data: {
+      email: "share.this.course@gmail.com",
+      name: "O Curso"
+    }
+  });
+  return user;
+}
+
+export async function createManyRecords() {
+  const users = [
+    { email: "preula@gmail.com", name: "Eitcha" },
+    { email: "eita@gmail.com", name: "Cavalo" },
+    { email: "lord@outlook.com", name: "Uii" },
+    { email: "cleiton@gmail.com", name: "Pedro" },
+    { email: "luch@gmail.com", name: "Luch" }
+  ];
+  for(let user of users) {
+    try {
+      await prisma.user.create({
+        data: user
+      });
+    } catch(err) {
+      console.log("Falha ao criar usuários");
+    }
+  }
+}
+
+export async function findSingleRecord(email: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email
+    }
+  });
+  return user;
+}
+
+export async function findAllRecords() {
+  return await prisma.user.findMany();
+}
+
+export async function updateSingleRecord(email: string, newName: string) {
+  const user = await prisma.user.update({
+    where: { 
+      email: email
+    },
+    data: {
+      name: newName
+    }
+  });
+  return user;
+}
+
+export async function updateManyRecords() {
+  await prisma.user.updateMany({
+    where: {
+      email: {
+        contains: "@gmail.com"
+      }
+    },
+    data: {
+      name: "HU3HU3HU3"
+    }
+  });
+}
+
+export async function updateOrCreate(email: string, name: string) {
+  await prisma.user.upsert({
+    where: {
+      email: email
+    }, 
+    create: {
+      email: email,
+      name: name
+    },
+    update: {
+      name: name
+    }
+  });
+}
+
+export async function deleteSingleRecord(email: string) {
+  await prisma.user.delete({
+    where: {
+      email: email
+    }
+  });
+}
+
+export async function deleteManyRecords() {
+  await prisma.user.deleteMany({
+    where: {
+      email: {
+        contains: "@gmail.com"
+      }
+    }
+  });
+}
+
+export async function deleteAllRecords() {
+  await prisma.user.deleteMany({});
 }
